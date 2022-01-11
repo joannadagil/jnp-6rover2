f66fdbbe8830191f1e18329cbf23ad0aebfbc2f9
MaciekPiechota <m.piechota7@student.uw.edu.pl>
2022-01-11 02:25:26 +0100

    Delete zadanie6clion directory

--- zadanie6clion/Rover.cc
+++
@@ -1,8 +0,0 @@
-#include "Rover.h"
-
-RoverBuilder Rover::create() { return RoverBuilder{}; }
-
-RoverBuilder& RoverBuilder::program_command(const char &id, Action action) {
-    return *this;
-}
-
--- zadanie6clion/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Rover.h"
-#include "RoverBuilder.h"
-using namespace std;
-int main() {
-    Rover p = Rover::create().program_command('a');
-    Rover q = RoverBuilder().program_command('a');
-    return 0;
-}
